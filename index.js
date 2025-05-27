b4d8ac8a2286114181ca99f0ff48d59f0dd152c7
maurice diong <mdiong00@gmail.com>
2025-05-27 04:56:48 -0400

    completed scope lab. couldn't use function expressions for some reasons

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-// Write your solution in this file!
-var customerName = `bob`;
+// // Write your solution in this file!
+// var customerName = `bob`;
 
-function upperCaseCustomerName () {
-    customerName = customerName.toUpperCase();
+// function upperCaseCustomerName () {
+//     customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer () {
+//     bestCustomer = `not bob`;
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = `maybe bob`;
+// }
+
+// const leastFavoriteCustomer = `mia`;
+
+// function changeLeastFavoriteCustomer () {
+//     leastFavoriteCustomer = `mama`;
+// }
+
+var customerName = 'bob'
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase()
 }
 
-function setBestCustomer () {
-    bestCustomer = `not bob`;
+function setBestCustomer (){
+    bestCustomer = 'not bob'
 }
 
-function overwriteBestCustomer() {
-    bestCustomer = `maybe bob`;
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
 }
 
-const leastFavoriteCustomer = `mia`;
+const leastFavoriteCustomer = 'sam'
 
-function changeLeastFavoriteCustomer () {
-    leastFavoriteCustomer = `mama`;
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bobby'
 }
